19810efd451ae74b6c1f3d2b920610802652062b
CalebRG <calebrg011@gmail.com>
2021-11-11 22:03:35 -0500

    gun and things working

--- Enemy/Enemy.gd
+++ Enemy/Enemy.gd
@@ -6,13 +6,18 @@ var target
 
 func _ready():
 	space_state = get_world().direct_space_state
+	$AnimationPlayer.play("Run")
+	$AnimationPlayer.play("Idle")
 
 func _physics_process(delta):
 	if target:
 		var result = space_state.intersect_ray(global_transform.origin, target.global_transform.origin)
 		if result.collider.is_in_group("Player"):
 			look_at(target.global_transform.origin, Vector3.UP)
+			$AnimationPlayer.play("Run")
 			move_to_target(delta)
+		else:
+			$AnimationPlayer.play("Idle")
 
 
 func _on_Area_body_entered(body):
@@ -23,8 +28,10 @@ func _on_Area_body_entered(body):
 func _on_Area_body_exited(body):
 	if body.is_in_group("Player"):
 		target = null
+		$AnimationPlayer.play("Idle")
 		print(body.name + " exited")
 		
+
 func move_to_target(delta):
 	var direction = (target.transform.origin - transform.origin).normalized()
 	move_and_slide(direction * speed * delta, Vector3.UP)
@@ -34,3 +41,7 @@ func set_color_red():
 
 func set_color_green():
 	null
+
+func _on_Killzone_body_entered(body):
+	if body.is_in_group("Bullet"):
+		queue_free()
